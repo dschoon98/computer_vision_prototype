a8b4ab96b76f20bd6815cdc9b610a035b4854611
TheChirc <quinten-hilten@hotmail.com>
2022-03-02 13:28:47 +0100

    test.py, random test matrix

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
 import numpy as np
 
+matrix = np.zeros([400,400], dtype=object)
 
 
-print("Hallo wereld")
+for i in range(400):
+    for j in range(400):
+        temp_array=np.zeros(3)
+        for k in range(3):
+            temp_array[k]=np.random.rand()
+
+        matrix[i,j]=temp_array
+
+
+print(matrix)
 
 
