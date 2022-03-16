e8e3fec824c097db92ebf13f39c02cb4f391fec1
isaironi <abinamboesjes@gmail.com>
2022-03-16 17:43:46 +0100

    did my alg on a bunch of images from cyberzoo

--- abinas_code_together.m
+++ abinas_code_together.m
@@ -1,27 +1,38 @@
+
 addpath .
 
-tic
-I = imread('/home/morgoth/computer_vision_prototype/bebop_images/cyberzoo_poles/20190121-135009/85878042.jpg'); % i dont understand paths
+
+
+
+
+
+myFolder = '/home/morgoth/computer_vision_prototype/bebop_images/cyberzoo_poles_panels/20190121-140205' %20190121-140205
+writeFolder = '/home/morgoth/computer_vision_prototype/CV_output/with_resize/'
+%I = imread('/home/morgoth/computer_vision_prototype/bebop_images/cyberzoo_poles/20190121-135009/85878042.jpg'); % i dont understand paths
+filePattern = fullfile(myFolder, '*.jpg');
+theFiles = dir(filePattern);
+
+
+for k = 1:length(theFiles)
+
+jpgFileName = strcat(myFolder, num2str(k), '.jpg');
+baseFileName = theFiles(k).name;
+fullFileName = fullfile(theFiles(k).folder, baseFileName);	  
+I = imread(fullFileName);
 bw = rgb2gray(I);
-% picture was sideways so needed to transpose :
 bw = bw' ;
-
-% may want subsample step ?
-% here's the subsampling you guys do:
-tic
-Im_smol = imresize(bw, 0.1)
-
-figure
-imshow(bw)
-
-%method #1 : Gaussian filter
-
-G_Im10 = imgaussfilt(bw, 2);
-Gauss_diff = bw - G_Im10;
+Im_smol = imresize(bw, 0.1) ; % check if resizing is bad, its not downsampled 
+G_Im10 = imgaussfilt(Im_smol, 2);
+Gauss_diff = Im_smol - G_Im10;
 idx = find(Gauss_diff > 10);
 Gauss_diff(idx) = 255 ;
 [Gauss_lines_min, Gauss_lines_max ] = grab_lines_2(Gauss_diff , 15 );
-toc
+together_im = [Im_smol ; Gauss_diff ; Gauss_lines_max] ;
+new_name = strcat(writeFolder , num2str(k) , '.jpg');
+imwrite(together_im, new_name);
+ 
+end
 
+ 
 % the best ones here are Gauss_lines_min or Gauss_lines_max. We should pick
 % one.
