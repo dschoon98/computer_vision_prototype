834009a393a582dcc5b3508d7176e270b486e3f0
TheChirc <quinten-hilten@hotmail.com>
2022-03-21 14:20:40 +0100

    fixed few bugs of testxray.c

--- testxray.c
+++ testxray.c
@@ -37,64 +37,6 @@ int test_matrix[22][37] = {
     };
 
 
-void x_ray(int bin_mat[rows][cols], int* k_object, float object_matrix[object_amount][14])
-{
-    int i_start = 0;
-    int i_end = rows;
-    int j_start = 0;
-    int j_end = cols;
-    bool running = true;
-
-    while (running)
-    {
-        bool stopvar = false;
-        for(int i = i_start; i<i_end;i++)
-        {
-            for(int j = j_start; j<j_end;j++)
-            {
-                if(bin_mat[i][j]==1)
-                {
-                    bool inside_object = false;
-                    for (int K = 0; K < k_object; K++)
-                    {
-                        if(i>=object_matrix[K][0] && i<=object_matrix[K][6] && j>=object_matrix[K][5] && j<=object_matrix[K][3])
-                        {
-                            inside_object=true;
-                            for(int z = 0; z< object_matrix[K][3]-j;z++)
-                            {
-                                K++;
-                            }
-                        }
-                        if(inside_object)
-                        {
-                            break;
-                        }
-                    }
-                    if(!inside_object)
-                    {
-                        object_matrix[k_object][0]=i;
-                        object_matrix[k_object][1]=j;
-                        stopvar=true;
-                        break;
-                    }
-                }
-            }
-            if(stopvar)
-            {
-                right_maxima_finder(bin_mat, object_matrix[k_object][0], object_matrix[k_object][1], k_object, object_matrix);
-                left_maxima_finder(bin_mat, object_matrix[k_object][0], object_matrix[k_object][1], k_object, object_matrix);
-                lower_maxima_finder(bin_mat, object_matrix[k_object][2], object_matrix[k_object][3], k_object, object_matrix);
-                i_start = object_matrix[k_object][0];
-                k_object+=1;
-                break;
-            }
-            if(j==cols-1&&i==rows-1)
-            {
-                running=false;
-            }
-        }
-    }
-}
 
 void right_maxima_finder(int bin_mat[rows][cols],int i, int j, int* k_object, float object_matrix[100][14 ] /*int object_matrix[100][14]*/)
 {   
@@ -192,8 +134,67 @@ void lower_maxima_finder(int bin_mat[rows][cols],int i, int j, int* k_object, fl
     object_matrix[*k_object][7] = local_j;
 }
 
+void x_ray(int bin_mat[rows][cols], int* k_object, float object_matrix[object_amount][14])
+{
+    int i_start = 0;
+    int i_end = rows;
+    int j_start = 0;
+    int j_end = cols;
+    bool running = true;
+
+    while (running)
+    {
+        bool stopvar = false;
+        for(int i = i_start; i<i_end;i++)
+        {
+            for(int j = j_start; j<j_end;j++)
+            {
+                if(bin_mat[i][j]==1)
+                {
+                    bool inside_object = false;
+                    for (int K = 0; K < *k_object; K++)
+                    {
+                        if(i>=object_matrix[K][0] && i<=object_matrix[K][6] && j>=object_matrix[K][5] && j<=object_matrix[K][3])
+                        {
+                            inside_object=true;
+                            for(int z = 0; z< object_matrix[K][3]-j;z++)
+                            {
+                                K++;
+                            }
+                        }
+                        if(inside_object)
+                        {
+                            break;
+                        }
+                    }
+                    if(!inside_object)
+                    {
+                        object_matrix[*k_object][0]=i;
+                        object_matrix[*k_object][1]=j;
+                        stopvar=true;
+                        break;
+                    }
+                }
+                if(j==cols-1&&i==rows-1)
+                {
+                    running=false;
+                }
+            }
+            if(stopvar)
+            {
+                right_maxima_finder(bin_mat, (int)object_matrix[*k_object][0], (int)object_matrix[*k_object][1], k_object, object_matrix);
+                left_maxima_finder(bin_mat, (int)object_matrix[*k_object][0], (int)object_matrix[*k_object][1], k_object, object_matrix);
+                lower_maxima_finder(bin_mat, (int)object_matrix[*k_object][2], (int)object_matrix[*k_object][3], k_object, object_matrix);
+                i_start = object_matrix[*k_object][0];
+                k_object+=1;
+                break;
+            }
+        }
+    }
+}
+
 int main() {
-    //x_ray(test_matrix,&k_object,object_matrix);
+    x_ray(test_matrix,&k_object,object_matrix);
     right_maxima_finder(test_matrix,3,9,&k_object,object_matrix);
     left_maxima_finder(test_matrix,3,9,&k_object,object_matrix);
     lower_maxima_finder(test_matrix,3,9,&k_object,object_matrix);
@@ -204,7 +205,7 @@ int main() {
     for(int i=0; i<10; i++) {
     // inner loop for column
     for(int j=0; j<8; j++) {
-      printf("%d ", object_matrix[i][j]);
+      printf("%f ", object_matrix[i][j]);
     }
     printf("\n"); // new line
   }
